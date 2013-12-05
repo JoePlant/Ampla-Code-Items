9d7b529952f469faf343b5e8978df68fc3a449c3
Joseph Plant <git@JosephPlant.com>
2013-12-05 16:13:34 +1000

    Added Ignore Old Samples

---
+++ Ignore Old Samples/Code.Conditions.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Diagnostics;
+using Citect.Ampla.Framework;
+using Citect.Ampla.Runtime.Data.Samples;
+using Citect.Ampla.Runtime.Data;
+using Citect.Common;
+
+namespace Code
+{
+
+	public static class Conditions
+	{
+		private static TimeSpan maxAge = TimeSpan.FromMinutes(5);
+		
+		public static Sample AllSamples(Sample sample)
+		{
+			return sample;
+		}
+			
+		public static Sample IgnoreOldSamples(Item item, Sample sample)
+		{
+			TimeSpan age = DateTime.UtcNow - sample.TimeStamp;
+			if (age < maxAge)
+			{
+				return sample;
+			}
+			
+			trace(item, "Ignoring old samples. Age={0:0.0} min", age.TotalMinutes);
+		
+			return new Sample(sample.SampleTypeCode, sample.TimeStamp, sample.Value, Quality.Bad) ;
+		}
+		
+		private static void trace(Item item, string format, params object[] args)
+        {
+			item.WriteTraceMessage(TraceLevel.Warning, format, args);
+        }
+		
+		private static void trace(string format, params object[] args)
+        {
+			Diagnostics.Write(TraceLevel.Info, format, args);
+        }
+
+	}
+}
+
---
+++ Ignore Old Samples/README.md
@@ -0,0 +1,30 @@
+Ignore Old Samples
+================
+
+This Code item will allow conditions to ignore old samples that are older than 5 minutes.
+
+Purpose
+===
+If you use a timer to trigger some event in Ampla such as an Action item or Plant2Business integration, it is possible to get multiple samples triggered at the same time especially when restarting the service or configuring an item for the first time.
+
+This can cause extra load on startup and cause duplicate records.
+
+How to use it
+===
+
+1. Add the code specified in the code item.
+2. Change the expression that is triggering the result as follows:
+	```Code.Conditions.IgnoreOldSamples(
+			this,
+			Enterprise.Site.Timer.Values[time]
+		)
+	```
+3. Set the TraceLevel property to ```Warning```
+
+How it works
+===
+When a sample is passed in for evaluation, this code will check the TimeStamp and make it ```Bad``` quality if it is more than 5 minutes old.
+
+If the TraceLevel of the item is set to ```Warning``` or ```Info``` then a message will be sent. 
+
+
