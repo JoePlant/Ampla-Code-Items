c97effc7bc9e6ebda8456cd269c20877a73d85f5
Joseph Plant <git@JosephPlant.com>
2015-11-11 15:55:18 +1000

    Added CalendarVariable

---
+++ Calendar/CalendarVariable.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using Citect.Ampla.Framework;
+using Citect.Ampla.Runtime.Data;
+using Citect.Ampla.Runtime.Data.Samples;
+using Citect.Ampla.Runtime.Data.Streams;
+
+namespace Code.Calendar
+{
+    /// <summary>
+    ///     Fluent interface for configuring Calendar Variables
+    /// </summary>
+    public interface IFluentCalendar
+    {
+        IFluentCalendar ForCalendar(string calendarName);
+        IFluentCalendar ForItem(Item item);
+        IFluentCalendar ForPeriod(TimeSpan period);
+        IFluentCalendar UseDefault(double defaultValue);
+        IFluentCalendar WithDependency(ISampleStream sampleStream);
+        IFluentCalendar WithWarning();
+        Sample GetSample(DateTime time);
+    }
+
+    /// <summary>
+    ///     Configure a Calendar Variable
+    /// </summary>
+    public class CalendarVariable : IFluentCalendar
+    {
+        private string calendarName;
+        private double defaultDouble;
+        private bool useDefault;
+        private bool warnOnNoValue;
+        private TimeSpan period;
+        private bool useRate;
+
+        /// <summary>
+        ///     Use the specified calendar
+        /// </summary>
+        /// <param name="calendar">The calendar.</param>
+        /// <returns></returns>
+        public IFluentCalendar ForCalendar(string calendar)
+        {
+            calendarName = calendar;
+            return this;
+        }
+
+        /// <summary>
+        ///     Use the Specified item's full name as the calendar
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public IFluentCalendar ForItem(Item item)
+        {
+            calendarName = item.FullName;
+            return this;
+        }
+
+        /// <summary>
+        ///     If the calendar is a rate, then calculate for the specified period
+        /// </summary>
+        /// <param name="ratePeriod">The rate period.</param>
+        /// <returns></returns>
+        public IFluentCalendar ForPeriod(TimeSpan ratePeriod)
+        {
+            period = ratePeriod;
+            useRate = true;
+            return this;
+        }
+
+        /// <summary>
+        ///     If no calendar value exists, then use the default value
+        /// </summary>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public IFluentCalendar UseDefault(double defaultValue)
+        {
+            defaultDouble = defaultValue;
+            useDefault = true;
+            return this;
+        }
+
+        /// <summary>
+        ///     References the stream dependency for triggering calculations
+        /// </summary>
+        /// <param name="sampleStream">The sample stream.</param>
+        /// <returns></returns>
+        public IFluentCalendar WithDependency(ISampleStream sampleStream)
+        {
+            // Expression  sampleStream is used to register the dependency
+            return this;
+        }
+
+        /// <summary>
+        ///     If the calendar is not set then write a warning message when calculating
+        /// </summary>
+        /// <returns></returns>
+        public IFluentCalendar WithWarning()
+        {
+            warnOnNoValue = true;
+            return this;
+        }
+
+        /// <summary>
+        ///     Gets a sample from calendar at the specified time
+        /// </summary>
+        /// <param name="time">Time for the sample</param>
+        /// <returns></returns>
+        public Sample GetSample(DateTime time)
+        {
+            object value =
+                useRate
+                    ? Citect.Ampla.General.Server.Calendar.CalendarCache.GetValue(calendarName, time, period)
+                    : Citect.Ampla.General.Server.Calendar.CalendarCache.GetValue(calendarName, time);
+
+            if (value == null || value is DBNull)
+            {
+                if (warnOnNoValue)
+                {
+                    Citect.Common.Diagnostics.Write(TraceLevel.Warning, "No calendar value for: '{0}'", calendarName);
+                }
+
+                if (useDefault)
+                {
+                    return new Sample(time, defaultDouble, Quality.GoodLocalOverride);
+                }
+                return new Sample(time, 0.0D, Quality.BadUninitialized);
+            }
+
+            double dValue = Convert.ToDouble(value);
+            return new Sample(time, dValue, Quality.Good);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,9 +4,16 @@ Ampla-Code-Items
 Collection of useful Code items for Ampla projects
 
 Code to ignore old samples
+
 * [Ignore Old Samples](/Ignore%20Old%20Samples)
 
-Code to allow 
+Code to create read Calendar values in a Calculated Variable. 
+
+* [Calendar](/Calendar)
+
+
+Code to support record changing
+
 * [Records](/Records)
 * [Custom](/Custom)
 * [Database](/Database)
