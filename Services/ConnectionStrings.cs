56bc49196efd1347cf38b3e831c52a4823fd6709
Joseph Plant <git@JosephPlant.com>
2015-12-22 22:47:13 +1000

    Updated connection strings

--- Services/ConnectionStrings.cs
+++ Services/ConnectionStrings.cs
@@ -12,5 +12,13 @@
             = "Data Source=localhost;" +
               "Initial Catalog=AmplaProjectExtras;" +
               "Trusted_Connection=True;";
+
+        /// <summary>
+        ///     Connection String for the SMS database
+        /// </summary>
+        public static readonly string SmsDatabase
+            = "Data Source=localhost;" +
+              "Initial Catalog=SMS;" +
+              "Trusted_Connection=True;";
     }
 }
